e2fcb8d852f59a7a93f5cc38b2921fdfa29f5904
Claiton Campos <eupe@icloud.com>
2023-06-29 11:57:01 -0400

    change ports 3306 6306

--- run.sh
+++ run.sh
@@ -19,7 +19,7 @@ docker run --rm -d --name ${CONTAINER_NAME} \
 
 # Run nginx container
 docker run -d --rm --name ${CONTAINER_NAME}_nginx \
-    -p 9000:80 \
+    -p 9001:80 \
     -v $(pwd)/docker/nginx.conf:/etc/nginx/conf.d/default.conf \
     -e VIRTUAL_HOST=${VIRTUAL_HOST} \
     -e LETSENCRYPT_HOST=${VIRTUAL_HOST} \
